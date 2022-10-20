077c908f523b038701a4cb1c0e1eac2225363cc1
John Canero <johncanero.official@gmail.com>
2022-10-21 01:09:45 +0800

    Change ico to png Favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,7 @@ import Head from 'next/head'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <>
-  <Head>
-        <link rel="icon" href="/favicon.ico" />
-  </Head>
+  return <> 
   <Component {...pageProps} />
   </>
   
