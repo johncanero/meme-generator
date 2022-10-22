183f8f5c752f8653a851f23069387797b622d637
John Canero <johncanero.official@gmail.com>
2022-10-22 08:25:09 +0800

    Meme Generator Header (Navbar) Progress

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import Header from '../components/Header'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -21,7 +21,7 @@ const Home: NextPage = () => {
  
 
       <main>
-
+        <Header />
       </main>
 
 
