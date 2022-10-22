4a50b3e32ca077f76841c17a5256a7312cb5eaed
John Canero <johncanero.official@gmail.com>
2022-10-22 12:03:18 +0800

    Add Image Meme Icon in Navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,22 +1,31 @@
-import { Fragment } from "react";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import React from "react";
+import Image from "next/image";
+import MemeIcon from "../public/images/memeIcon.png";
 
-export default function Example() {
+const Header = () => {
   return (
     <div className="bg-gradient-to-r from-memeYellow to-memeOrange font-Poppins">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-20 items-center justify-between">
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex flex-shrink-0 items-center">
-              <h1 className="text-memeViolet font-semibold text-2xl">Meme Generator</h1>
+              <div className="mr-3">
+                <Image height={35} width={40} src={MemeIcon} alt="meme-icon" />
+              </div>
+              <h1 className="text-memeViolet font-semibold text-2xl">
+                Meme Generator
+              </h1>
             </div>
           </div>
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            <h2 className="text-memeViolet font-medium hidden md:block">React Course - Project 3</h2>
+            <h2 className="text-memeViolet font-medium hidden md:block">
+              React Course - Project 3
+            </h2>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
+
+export default Header;
