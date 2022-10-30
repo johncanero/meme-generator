448ad9408073204305a00ade120161bcddf013a4
John Canero <johncanero.official@gmail.com>
2022-10-30 18:53:21 +0800

    Imput Text Function and Styling

--- components/Meme.tsx
+++ components/Meme.tsx
@@ -5,28 +5,14 @@ import { MdFilterFrames } from "@react-icons/all-files/md/MdFilterFrames";
 import memesData from "../memesData";
 
 const Meme = () => {
-  // React useState
-  // const [memes, setMemes] = React.useState(["Meme 1", "Meme 2"]);
-
-  // const addItem = () => {
-  //   const newMemeText = `Meme ${memes.length + 1}`;
-  //   setMemes((prevState) => [...prevState, newMemeText]);
-  // };
-
-  // const memeElements = memes.map((meme) => <p key={meme}>{meme}</p>);
-
-  // Refactor State
-  // const [memeImage, setMemeImage] = React.useState("");
-
   const [meme, setMeme] = React.useState({
     topText: "",
     bottomText: "",
-    randomImage: "https://i.imgflip.com/3i7p.jpg",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
   });
-
   const [allMemeImages, setAllMemeImages] = React.useState(memesData);
 
-  const getMemeImage =() => {
+  const getMemeImage = () => {
     const memesArray = allMemeImages.data.memes;
     const randomNumber = Math.floor(Math.random() * memesArray.length);
     const url = memesArray[randomNumber].url;
@@ -34,39 +20,37 @@ const Meme = () => {
       ...prevMeme,
       randomImage: url,
     }));
-  }
+  };
 
-  // Counter - useState
-  // const [thingsArray, setThingsArray] = React.useState(["Thing 1", "Thing 2"]);
-
-  // const addItem = () => {
-  //   setThingsArray((prevThingsArray) => {
-  //     return [...prevThingsArray, `Thing ${prevThingsArray.length + 1}`];
-  //   });
-  // };
-
-  // const thingsElements = thingsArray.map((thing) => <p key={thing}>{thing}</p>);
+  const handleChange = (event: { target: { name: any; value: any } }) => {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
+  };
 
   return (
     <div>
       <div className="flex flex-col items-center md:flex-row md:justify-center mt-10">
         <input
           type="text"
-          id="first_name"
+          placeholder="Top text"
           className="bg-gray-50 border-2 border-gray-200 text-memeDarkMagenta text-sm font-medium rounded-md focus:ring-memeDarkMagenta focus:border-memeDarkMagenta block w-80 p-3 md:w-64 md:mx-3"
-          placeholder="Top Text"
-          required
+          name="topText"
+          value={meme.topText}
+          onChange={handleChange}
         ></input>
 
         <input
           type="text"
-          id="first_name"
+          placeholder="Bottom text"
           className="bg-gray-50 border-2 border-gray-200 text-memeDarkMagenta text-sm font-medium rounded-md focus:ring-memeDarkMagenta focus:border-memeDarkMagenta block w-80 p-3 mt-5 md:mt-0 md:w-64 md:mx-3"
-          placeholder="Bottom Text"
-          required
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={handleChange}
         ></input>
       </div>
-
 
       <button
         className="flex mx-auto  bg-gradient-to-r from-memeYellow to-memeOrange text-memeViolet hover:from-memeOrange hover:to-memeYellow active:bg-violet-700 font-bold py-3 border-2 cursor-pointer border-memeYellow text-lg md:text-xl rounded-lg px-12 mt-6 md:px-36"
@@ -76,9 +60,11 @@ const Meme = () => {
         <MdFilterFrames className="mt-1 ml-1 align-middle" />
       </button>
 
-      {/* <div className="text-center mt-12">{memeElements}</div> */}
-      <div className="flex justify-center mx-auto mt-6 md:mt-9 md:mb-28 sm:w-80 md:w-128 lg:h-120">
+      {/* Image */}
+      <div className="flex justify-center mx-auto mt-6 relative md:mt-9 md:mb-28 sm:w-80 md:w-128 lg:h-120">
         <Image width={600} height={500} src={meme.randomImage} />
+        <h2 className="absolute flex justify-center p-5 font-Poppins font-bold uppercase text-4xl text-white top-0">{meme.topText}</h2>
+        <h2 className="absolute flex justify-center p-5 font-Poppins font-bold uppercase text-4xl text-white bottom-0">{meme.bottomText}</h2>
       </div>
     </div>
   );
